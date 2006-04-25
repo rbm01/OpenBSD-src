e4fdc6ab06a5d913c8bb8b63069ef8ceb36120df
miod <miod@>
2006-04-25 15:54:15 +0000

    Oops, correct import this time.

--- sys/arch/aviion/aviion/conf.c
+++ sys/arch/aviion/aviion/conf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.c,v 1.1 2006/04/25 15:54:13 miod Exp $	*/
+/*	$OpenBSD: conf.c,v 1.1.1.1 2006/04/25 15:54:13 miod Exp $	*/
 
 /*-
  * Copyright (c) 1991 The Regents of the University of California.
