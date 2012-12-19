4f46561324a4b134d651a8c7c0d183a706ba17ce
jj <jj@>
2012-12-19 18:51:03 +0000

    enter -stable. ok deraadt@, reported by Mitja M.

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.120 2012/07/16 10:50:07 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.120.2.1 2012/12/19 18:51:03 jj Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -64,7 +64,7 @@ ost="OpenBSD"
 osr="5.2"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
+#define STATUS "-stable"
 #if 0
 #define STATUS "-beta"			/* just before a release */
 #define STATUS "-current"		/* just after a release */
