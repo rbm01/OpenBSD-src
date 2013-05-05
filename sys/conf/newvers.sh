dc2059cafed96b2a327b52e90129feb4747b6bc4
sthen <sthen@>
2013-05-05 19:41:53 +0000

    switch to -stable suffix, reminded by mitja

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.123 2013/02/21 15:26:20 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.123.2.1 2013/05/05 19:41:53 sthen Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -68,7 +68,7 @@ cat >vers.c <<eof
 #define STATUS "-current"		/* just after a release */
 #define STATUS "-beta"			/* just before a release */
 #endif
-#define STATUS ""			/* release */
+#define STATUS "-stable"
 
 const char ostype[] = "${ost}";
 const char osrelease[] = "${osr}";
