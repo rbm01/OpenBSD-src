205d4ea86ea7c5b75052ecf1ac075e60f718b712
djm <djm@>
2014-04-21 00:30:48 +0000

    MFC:
    reliability fix for OpenSSH using curve25519-sha256@libssh.org key
    exchange method.
    
    revision 1.71
    date: 2014/04/18 23:52:25;  author: djm;  state: Exp;  lines: +2 -2;
    OpenSSH 6.5 and 6.6 have a bug that causes ~0.2% of connections
    using the curve25519-sha256@libssh.org KEX exchange method to fail
    when connecting with something that implements the spec properly.
    
    Disable this KEX method when speaking to one of the affected
    versions.
    
    revision 1.57
    date: 2014/04/16 23:22:45;  author: djm;  state: Exp;  lines: +4 -1;
    skip leading zero bytes in buffer_put_bignum2_from_string();
    reported by jan AT mojzis.com; ok markus@

--- usr.bin/ssh/version.h
+++ usr.bin/ssh/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.70 2014/02/27 22:57:40 djm Exp $ */
+/* $OpenBSD: version.h,v 1.70.4.1 2014/04/21 00:30:48 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_6.6"
+#define SSH_VERSION	"OpenSSH_6.6.1"
