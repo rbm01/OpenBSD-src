0787455155d38ae5c967d2d8980b6ffb610d4d24
jsg <jsg@>
2014-09-07 03:07:16 +0000

    5.6-stable

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.133 2014/07/29 12:56:41 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.133.4.1 2014/09/07 03:07:16 jsg Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -64,7 +64,7 @@ ost="OpenBSD"
 osr="5.6"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
+#define STATUS "-stable"
 #if 0
 #define STATUS "-beta"			/* just before a release */
 #define STATUS "-current"		/* just after a release */
