923c4d0890a3486c9b017143e37f21639d6842a3
benno <benno@>
2015-09-20 11:20:16 +0000

    maintainance diff for relayd MFC the following changes
    - Missing free(3) in error path (ssl.c,v 1.29)
    - fix a memory leak. (pfe.c,v 1.80)
    - allocate se_log evbuffer before loging errors with relay_close()
      (relay.c,v 1.192)
    - fix a file descriptor leak in http protocol handling
      (relay.c,v 1.193 and relay_http.c,v 1.44)
    - Fix obvious problems with relayd config reload
      (ca.c,v 1.13; config.c,v 1.25; parse.y,v 1.204;
      relayd.c,v 1.139; relayd.h,v 1.209)
    - http protocol: you cannot append to the previous key-value before
      line three of a request (relay_http.c,v 1.45)
    - fix a crash / use after free (relay.c,v 1.194; relay_http.c,v 1.46)
    - fix a non safe use of TAILQ_FOREACH with TAILQ_REMOVE
      (relay_http.c,v 1.47)
    - Plug a memory leak by simplifying kv_free() (relayd.c,v 1.141)
    - Fix memory leak in error case (relay_http.c,v 1.48)
    - track the connection state of a session and stops doing double
      opens in certain situations (relay.c,v 1.195; relay_http.c,v 1.49; relayd.h,v 1.210)
    - coding style (relay.c,v 1.196; relay_http.c,v 1.50; relayd.h,v 1.212)
    
    ok claudio@, sthen@ and feedback tedu@

--- usr.sbin/relayd/config.c
+++ usr.sbin/relayd/config.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: config.c,v 1.24 2015/01/22 17:42:09 reyk Exp $	*/
+/*	$OpenBSD: config.c,v 1.24.2.1 2015/09/20 11:20:16 benno Exp $	*/
 
 /*
  * Copyright (c) 2011 - 2014 Reyk Floeter <reyk@openbsd.org>
@@ -142,7 +142,7 @@ config_purge(struct relayd *env, u_int reset)
 
 	if (what & CONFIG_TABLES && env->sc_tables != NULL) {
 		while ((table = TAILQ_FIRST(env->sc_tables)) != NULL)
-			purge_table(env->sc_tables, table);
+			purge_table(env, env->sc_tables, table);
 		env->sc_tablecount = 0;
 	}
 	if (what & CONFIG_RDRS && env->sc_rdrs != NULL) {
