b17cdb59b4e44ac7511d957894193689d582c41d
sthen <sthen@>
2016-01-14 14:48:18 +0000

    MFC changes by: deraadt@cvs.openbsd.org 2016/01/14 07:34:34
    Disable experimental client-side roaming support.  Server side was
    disabled/gutted for years already, but this aspect was surprisingly
    forgotten.
    Thanks for report from Qualys

--- usr.bin/ssh/ssh.c
+++ usr.bin/ssh/ssh.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssh.c,v 1.420.2.1 2016/01/14 11:50:37 sthen Exp $ */
+/* $OpenBSD: ssh.c,v 1.420.2.2 2016/01/14 14:48:18 sthen Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
@@ -1876,9 +1876,6 @@ ssh_session2(void)
 		} else
 			fork_postauth();
 	}
-
-	if (options.use_roaming)
-		request_roaming();
 
 	return client_loop(tty_flag, tty_flag ?
 	    options.escape_char : SSH_ESCAPECHAR_NONE, id);
