d7f114f1616e9f6efd6bf86569fc5d9c4c383c2d
guenther <guenther@>
2016-07-14 03:37:42 +0000

    backport 1.129
    ufs_readdir() buffer was meant to be limited to 64kB; reversed test
    permitted very big mallocs to panic the kernel.
    
    reported by Tim Newsham
    ok millert@

--- sys/ufs/ufs/ufs_vnops.c
+++ sys/ufs/ufs/ufs_vnops.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ufs_vnops.c,v 1.121 2015/04/17 04:43:21 guenther Exp $	*/
+/*	$OpenBSD: ufs_vnops.c,v 1.121.4.1 2016/07/14 03:37:42 guenther Exp $	*/
 /*	$NetBSD: ufs_vnops.c,v 1.18 1996/05/11 18:28:04 mycroft Exp $	*/
 
 /*
@@ -1459,7 +1459,7 @@ ufs_readdir(void *v)
 	 */
 
 	/* read from disk, stopping on a block boundary, max 64kB */
-	readcnt = max(count, 64*1024) - entries;
+	readcnt = min(count, 64*1024) - entries;
 
 	auio = *uio;
 	auio.uio_iov = &aiov;
