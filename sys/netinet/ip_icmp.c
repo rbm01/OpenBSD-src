f7913fff0371cdf1103ff0d4d4f5e5aaf1d80ced
bluhm <bluhm@>
2017-05-11 11:22:30 +0000

    If m is not a continuous mbuf cluster, m_pullup() in pr_input may change the pointer.  Then *mp keeps the invalid pointer and it might be used.  Fix the potential use after free in igmp_input_if() and icmp_input_if().  As the input buffers normally deal with mbuf clusters, the use after free should not happen. OK mikeb@ mpi@

--- sys/netinet/ip_icmp.c
+++ sys/netinet/ip_icmp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_icmp.c,v 1.163 2017/02/07 22:30:16 jmatthew Exp $	*/
+/*	$OpenBSD: ip_icmp.c,v 1.163.4.1 2017/05/11 11:22:30 bluhm Exp $	*/
 /*	$NetBSD: ip_icmp.c,v 1.19 1996/02/13 23:42:22 christos Exp $	*/
 
 /*
@@ -354,7 +354,7 @@ icmp_input_if(struct ifnet *ifp, struct mbuf **mp, int *offp, int proto)
 		goto freeit;
 	}
 	i = hlen + min(icmplen, ICMP_ADVLENMIN);
-	if (m->m_len < i && (m = m_pullup(m, i)) == NULL) {
+	if (m->m_len < i && (m = *mp = m_pullup(m, i)) == NULL) {
 		icmpstat_inc(icps_tooshort);
 		return IPPROTO_DONE;
 	}
@@ -476,7 +476,7 @@ icmp_input_if(struct ifnet *ifp, struct mbuf **mp, int *offp, int proto)
 				icmpstat_inc(icps_badlen);
 				goto freeit;
 			} else {
-				if ((m = m_pullup(m, (ip->ip_hl << 2) +
+				if ((m = *mp = m_pullup(m, (ip->ip_hl << 2) +
 				    ICMP_V6ADVLEN(icp))) == NULL) {
 					icmpstat_inc(icps_tooshort);
 					return IPPROTO_DONE;
