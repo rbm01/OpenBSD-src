8b07f46a9328736d18f12a6bfa800d38c85e839c
bluhm <bluhm@>
2017-08-01 21:58:24 +0000

    Both syslog(3) and syslogd(8) truncate the message at 8192 bytes. Do the same in sendsyslog(2). reported by Ilja Van Sprundel errata 016

--- sys/kern/subr_log.c
+++ sys/kern/subr_log.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: subr_log.c,v 1.50 2017/03/27 11:45:49 bluhm Exp $	*/
+/*	$OpenBSD: subr_log.c,v 1.50.4.1 2017/08/01 21:58:24 bluhm Exp $	*/
 /*	$NetBSD: subr_log.c,v 1.11 1996/03/30 22:24:44 christos Exp $	*/
 
 /*
@@ -417,6 +417,9 @@ dosendsyslog(struct proc *p, const char *buf, size_t nbyte, int flags,
 	struct uio auio;
 	size_t i, len;
 	int error;
+
+	if (nbyte > 8192)
+		nbyte = 8192;
 
 	/* Global variable syslogf may change during sleep, use local copy. */
 	fp = syslogf;
