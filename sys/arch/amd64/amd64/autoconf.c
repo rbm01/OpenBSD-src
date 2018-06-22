d065370147d51cab7e9c745db5d2a6dcfc6cb411
bluhm <bluhm@>
2018-06-22 13:05:33 +0000

    Merge the eager-FPU changes from 6.3-stable to 6.2: Intel CPUs speculatively access FPU registers even when FPU is disabled, so data (including AES keys) from previous contexts could be discovered if using lazy-save approach. backport from hshoexer@ OpenBSD 6.2 errata 017

--- sys/arch/amd64/amd64/autoconf.c
+++ sys/arch/amd64/amd64/autoconf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: autoconf.c,v 1.49 2017/06/20 21:05:46 deraadt Exp $	*/
+/*	$OpenBSD: autoconf.c,v 1.49.2.1 2018/06/22 13:05:33 bluhm Exp $	*/
 /*	$NetBSD: autoconf.c,v 1.1 2003/04/26 18:39:26 fvdl Exp $	*/
 
 /*-
@@ -137,10 +137,6 @@ cpu_configure(void)
 #endif
 
 	unmap_startup();
-
-#ifdef MULTIPROCESSOR
-	cpu_init_idle_pcbs();
-#endif
 
 	lcr8(0);
 	spl0();
