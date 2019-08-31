124fc908e3d3112b31c9ce949e40732653fcba8d
bluhm <bluhm@>
2019-08-31 23:51:23 +0000

    Restore msrs & pat on main cpu.  jcs observed a slow console on x1r7, and kettenis identified missing pat restore. Many other machines may be improved by this change. from deraadt; ok kettenis jcs
    OpenBSD 6.5 errata 009

--- sys/arch/amd64/amd64/acpi_machdep.c
+++ sys/arch/amd64/amd64/acpi_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: acpi_machdep.c,v 1.86 2018/10/23 17:51:32 kettenis Exp $	*/
+/*	$OpenBSD: acpi_machdep.c,v 1.86.2.1 2019/08/31 23:51:23 bluhm Exp $	*/
 /*
  * Copyright (c) 2005 Thorsten Lockert <tholo@sigmasoft.com>
  *
@@ -382,6 +382,8 @@ acpi_sleep_clocks(struct acpi_softc *sc, int state)
 void
 acpi_resume_clocks(struct acpi_softc *sc)
 {
+	cpu_init_msrs(&cpu_info_primary);
+
 #if NISA > 0
 	i8259_default_setup();
 #endif
