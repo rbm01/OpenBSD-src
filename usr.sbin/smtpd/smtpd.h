cd7e3af1fa04cd7f6a57048ed2b9862096418a97
millert <millert@>
2020-02-24 16:20:06 +0000

    OpenBSD 6.5 errata 031, February 24, 2020:
    An out of bounds read in smtpd allows an attacker to inject arbitrary
    commands into the envelope file which are then executed as root.
    Separately, missing privilege revocation in smtpctl allows arbitrary
    commands to be run with the _smtpq group.

--- usr.sbin/smtpd/smtpd.h
+++ usr.sbin/smtpd/smtpd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: smtpd.h,v 1.621 2019/04/08 08:22:32 eric Exp $	*/
+/*	$OpenBSD: smtpd.h,v 1.621.2.1 2020/02/24 16:20:06 millert Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@poolp.org>
@@ -97,8 +97,9 @@
 #define MTA_EXT_DSN		0x400
 
 
-#define P_NEWALIASES	0
-#define P_MAKEMAP	1
+#define P_SENDMAIL	0
+#define P_NEWALIASES	1
+#define P_MAKEMAP	2
 
 #define	CERT_ERROR	-1
 #define	CERT_OK		 0
