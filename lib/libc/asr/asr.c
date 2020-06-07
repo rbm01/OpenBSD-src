6c7da7b52556247337c830ff554cef1247967b80
deraadt <deraadt@>
2020-06-07 16:16:17 +0000

    In the libc resolver function asr_run(), clear the result buffer everytime, because there are callers who were inspecting unrelated fields. discussion with eric, otto, solution from semarie this is errata 6.6/031_asr and 6.7/009_asr

--- lib/libc/asr/asr.c
+++ lib/libc/asr/asr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: asr.c,v 1.61 2018/10/22 17:31:24 krw Exp $	*/
+/*	$OpenBSD: asr.c,v 1.61.4.1 2020/06/07 16:16:17 deraadt Exp $	*/
 /*
  * Copyright (c) 2010-2012 Eric Faurot <eric@openbsd.org>
  *
@@ -142,6 +142,8 @@ int
 asr_run(struct asr_query *as, struct asr_result *ar)
 {
 	int	r, saved_errno = errno;
+
+	memset(ar, 0, sizeof(*ar));
 
 	DPRINT("asr: asr_run(%p, %p) %s ctx=[%p]\n", as, ar,
 	    _asr_querystr(as->as_type), as->as_ctx);
