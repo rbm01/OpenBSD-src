70399392dabed0076e0b6c4da7796016a2225d1f
bluhm <bluhm@>
2020-10-26 21:06:03 +0000

    6.8-stable

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.177 2020/09/25 05:12:39 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.177.4.1 2020/10/26 21:06:03 bluhm Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -71,7 +71,7 @@ ost="OpenBSD"
 osr="6.8"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
+#define STATUS "-stable"
 #if 0
 #define STATUS "-beta"			/* just before a release */
 #define STATUS "-current"		/* just after a release */
