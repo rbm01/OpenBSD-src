ef04c4a7b1720c432fc20187220f231c9cfa9ff1
benno <benno@>
2021-11-09 13:41:24 +0000

    rpki-client(8) should handle CA misbehaviours as soft-errors.
    This is a merge of usr.sbin/rpki-client and usr.bin/rsync from current
    and includes all commits in rpki-client 7.5 up to Tue Nov 9 11:03:40
    2021 and to openrsync up to Wed Nov 3 14:42:13 2021, including:
    
    * Make rpki-client more resilient regarding untrusted input:
      - fail repository synchronisation after 15min runtime
      - limit the number of publication points per TAL
      - don't allow DOCTYPE definitions in RRDP XML files
      - fix detection of HTTP redirect loops.
    * limit the number of concurrent rsync processes.
    * fix CRLF in tal files.
    
    This is patches/6.9/common/021_rpki.patch.sig

--- usr.bin/rsync/session.c
+++ usr.bin/rsync/session.c
@@ -1,4 +1,4 @@
-/*	$Id: session.c,v 1.7 2019/05/08 20:00:25 benno Exp $ */
+/*	$Id: session.c,v 1.7.4.1 2021/11/09 13:41:24 benno Exp $ */
 /*
  * Copyright (c) 2019 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -14,7 +14,6 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-#include <sys/param.h>
 
 #include <assert.h>
 #include <stdint.h>
